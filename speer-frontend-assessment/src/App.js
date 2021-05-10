af9915537f4871f534b36624fee407bc42a815c8
Mugiesshan Anandarajah <manandarajah@users.noreply.github.com>
2021-05-10 18:42:58 -0400

    Properly positioned gradient buttons

--- speer-frontend-assessment/src/App.js
+++ speer-frontend-assessment/src/App.js
@@ -139,7 +139,7 @@ function App() {
               <Carousel.Caption>
                 <h3>First slide label</h3>
                 <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                <GradientButton id="herobtn" onMouseMove={hoverButton}><span>Pricing</span></GradientButton>
+                <GradientButton id="herobtn" style={{marginLeft: 500}} onMouseMove={hoverButton}><span>Pricing</span></GradientButton>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item interval={10000}>
@@ -152,7 +152,7 @@ function App() {
               <Carousel.Caption>
                 <h3>Second slide label</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <GradientButton id="herobtn" onMouseMove={hoverButton}><span>Pricing</span></GradientButton>
+                <GradientButton id="herobtn" style={{marginLeft: 500}} onMouseMove={hoverButton}><span>Pricing</span></GradientButton>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item interval={10000}>
@@ -165,7 +165,7 @@ function App() {
               <Carousel.Caption>
                 <h3>Third slide label</h3>
                 <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                <GradientButton id="herobtn" onMouseMove={hoverButton}><span>Pricing</span></GradientButton>
+                <GradientButton id="herobtn" style={{marginLeft: 500}} onMouseMove={hoverButton}><span>Pricing</span></GradientButton>
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
